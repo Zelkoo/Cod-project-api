bb17a90488c02b2f2b6d3c4a01e0970f4748cae7
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2022-12-28 00:24:25 +0100

    add routing/nav menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,34 @@ import { AppComponent } from './app.component';
 import { DataService } from "../data.service";
 import { HttpClientModule } from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
+import {MenuNavigationComponent} from "../menu/menu-component";
+import {RouterModule, Routes} from "@angular/router";
+import {MatchComponent} from "../match-component/match-component";
+import {WeaponsComponent} from "../weapons-component/weapons-component";
+import {OverviewComponent} from "../overview-component/overview-component";
+
+const routes: Routes = [
+  { path: 'match-component', component: MatchComponent },
+  { path: 'weapons-component', component: WeaponsComponent },
+  { path: 'overview-component', component: OverviewComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuNavigationComponent,
+    MatchComponent,
+    WeaponsComponent,
+    OverviewComponent
   ],
     imports: [
         BrowserModule,
         HttpClientModule,
         FormsModule,
+        RouterModule.forRoot(routes)
 
     ],
+  exports: [RouterModule],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
