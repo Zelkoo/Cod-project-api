9e558acd6e8e96c5f4fa070810ed5d16c5bc555c
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2022-12-28 00:23:48 +0100

    add weapons/overview/match component

---
+++ src/menu/menu-component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'nav-menu',
+  templateUrl: './menu-component.html',
+  styleUrls: ['./menu-component.css'],
+})
+
+export class MenuNavigationComponent {
+  constructor() {
+}
+};
---
+++ src/weapons-component/weapons-component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'weapons-component',
+  templateUrl: './weapons-component.html',
+  styleUrls: ['./weapons-component.css'],
+})
+
+export class WeaponsComponent {
+  constructor() {
+  }
+};
