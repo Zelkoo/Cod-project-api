8022ff059ba611baec6fa75684289fe72cbfb0f4
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2023-01-01 20:00:27 +0100

    weapon-component sketch

--- src/weapons-component/weapons-component.ts
+++ src/weapons-component/weapons-component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
 
 
 @Component({
@@ -7,7 +8,10 @@ import {Component} from '@angular/core';
   styleUrls: ['./weapons-component.css'],
 })
 
-export class WeaponsComponent {
+export class WeaponsComponent implements OnInit {
+
   constructor() {
   }
-};
+
+  ngOnInit() {}
+}
