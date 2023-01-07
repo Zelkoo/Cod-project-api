3b24d06a34653aca4356b8ec2d3ef51a5a4cc1ba
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2023-01-08 00:08:42 +0100

    no message

--- karma.conf.js
+++ karma.conf.js
@@ -32,12 +32,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
   },
-    reporters: ['junit'],
-    junitReporter: {
-      outputDir: process.env.JUNIT_REPORT_PATH,
-      outputFile: process.env.JUNIT_REPORT_NAME,
-      useBrowserName: false,
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
