cb5008868b64ca852c633d9bbc75fa93737deff6
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2023-01-07 20:30:05 +0100

    test: build on ci should fail

--- src/match-component/match-component.ts
+++ src/match-component/match-component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 
-@Component({
+@Componen({
   selector: 'match-component',
   templateUrl: './match-component.html',
   styleUrls: ['./match-component.css'],
