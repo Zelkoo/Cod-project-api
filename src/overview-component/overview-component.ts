72cb43b6bdd27019ac0e6afa8f324bfae7350b9e
DESKTOP-3NA4U74\Zuoo <zuogamer@gmail.com>
2023-02-05 23:54:06 +0100

    refactor

--- src/overview-component/overview-component.ts
+++ src/overview-component/overview-component.ts
@@ -1,8 +1,7 @@
-import {Component, Input, OnDestroy, OnInit, SimpleChange, SimpleChanges} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {DataService} from "../data.service";
 import {forkJoin, Subscription} from "rxjs";
 import {OverviewDataType} from "../helpers/overview-enum";
-import * as d3 from 'd3';
 import {AppComponent} from "../app/app.component";
 
 @Component({
@@ -12,68 +11,38 @@ import {AppComponent} from "../app/app.component";
 })
 
 export class OverviewComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
   @Input() assaultRifleData;
   kdRatio: number;
   kills: number;
-  wins: number;
-  losses: number;
-  bestKillStreak: number;
   deaths: number;
   headshots: number;
-  assists: number;
-  scorePerMinute: number;
-  scorePerGame: number;
-  totalShots: number;
   timePlayedTotal: number
   totalGamesPlayed: number
   winLossRatio: number
-
+  private subscription: Subscription;
 
   constructor(private readonly dataService: DataService, public app: AppComponent) {
   }
-  onMenuToggle(isOpen: boolean) {
-    console.log(isOpen);
-  }
+
+
   ngOnInit() {
 
     // this.app.loadAssaultRifleData()
     this.subscription = forkJoin([
-      this.dataService.getOverviewData(OverviewDataType.ScorePerMinute),
-      this.dataService.getOverviewData(OverviewDataType.ScorePerGame),
-      this.dataService.getOverviewData(OverviewDataType.TotalShots),
-      this.dataService.getOverviewData(OverviewDataType.Wins),
       this.dataService.getOverviewData(OverviewDataType.KDRatio),
-      this.dataService.getOverviewData(OverviewDataType.Kills),
-      this.dataService.getOverviewData(OverviewDataType.Assists),
-      this.dataService.getOverviewData(OverviewDataType.Headshots),
-      this.dataService.getOverviewData(OverviewDataType.Deaths),
-      this.dataService.getOverviewData(OverviewDataType.BestKillStreak),
-      this.dataService.getOverviewData(OverviewDataType.Losses),
       this.dataService.getOverviewData(OverviewDataType.TimePlayed),
       this.dataService.getOverviewData(OverviewDataType.TotalGamePlayed),
       this.dataService.getOverviewData(OverviewDataType.WinLoseRatio)
-    ]).subscribe(([scorePerMinute, scorePerGame, totalShots, wins, kdRatio, kills, assists, headshots, deaths, bestKillStreak, losses, timePlayedTotal, totalGamesPlayed, winLossRatio]
+    ]).subscribe(([kdRatio, timePlayedTotal, totalGamesPlayed, winLossRatio]
     ) => {
-      this.scorePerMinute = scorePerMinute.toFixed(2);
-      this.scorePerGame = scorePerGame.toFixed(2);
-      this.totalShots = totalShots;
-      this.wins = wins;
       this.kdRatio = kdRatio.toFixed(2);
-      this.kills = kills;
-      this.assists = assists;
-      this.headshots = headshots;
-      this.deaths = deaths;
-      this.bestKillStreak = bestKillStreak;
-      this.losses = losses;
       this.timePlayedTotal = Number((timePlayedTotal / 60 / 60).toFixed(0));
       this.totalGamesPlayed = totalGamesPlayed;
       this.winLossRatio = winLossRatio.toFixed(2);
     });
   }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
-    const svg = d3.select('svg');
-    svg.remove();
   }
 };
--- src/weapons-component/weapons-component.ts
+++ src/weapons-component/weapons-component.ts
@@ -3,8 +3,6 @@ import {DataService} from "../data.service";
 import {DonutChartService} from "../pie-chart-service/donut-chart-service";
 import {forkJoin, Subscription} from "rxjs";
 import {OverviewDataType} from "../helpers/overview-enum";
-import {PistolData} from "../helpers/data-interface";
-
 
 @Component({
   selector: 'weapons-component',
@@ -13,17 +11,21 @@ import {PistolData} from "../helpers/data-interface";
 })
 
 export class WeaponsComponent implements OnInit {
-  private subscription: Subscription;
+  mouseOver = false;
   deaths: number;
   kills: number;
   wins: number;
   losses: number;
   dataWinLosses: any[]
   dataKillDeaths: any[]
+  private subscription: Subscription;
+
   constructor(public donutChart: DonutChartService, public dataService: DataService) {
   }
 
   ngOnInit() {
+    const body = document.querySelector('body')
+    body.style.backgroundImage = ''
     this.subscription = forkJoin([
       this.dataService.getOverviewData(OverviewDataType.Wins),
       this.dataService.getOverviewData(OverviewDataType.Kills),
@@ -37,22 +39,26 @@ export class WeaponsComponent implements OnInit {
       this.kills = kills
       this.losses = losses
       this.dataWinLosses = [
-        { label: 'Losses', count: this.losses },
-          { label: 'Wins', count: this.wins },
-    ]
+        {label: 'Losses', count: this.losses},
+        {label: 'Wins', count: this.wins},
+      ]
       this.dataKillDeaths = [
-        { label: 'Deaths', count: this.deaths },
-        { label: 'Kills', count: this.kills },
+        {label: 'Deaths', count: this.deaths},
+        {label: 'Kills', count: this.kills},
       ]
     });
-      setTimeout(() => {
-        this.donutChart.createChart( '.donut', this.dataWinLosses )
-        this.donutChart.createChart('.donut2', this.dataKillDeaths)
-      }, 100)
+    setTimeout(() => {
+      this.donutChart.createChart('.chart-win-lose', this.dataWinLosses)
+      this.donutChart.createChart('.chart-kd-ratio', this.dataKillDeaths)
+    }, 100)
+  }
 
 
+  onMouseOver() {
+    this.mouseOver = true;
+  }
 
-
-
+  onMouseOut() {
+    this.mouseOver = false;
   }
 }
